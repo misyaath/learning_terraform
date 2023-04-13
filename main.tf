1b5f2fd662d3b3d96a5c0e7ad87cd466feaf1fc2
Mohammed Misyaath <mmisyaath@yahoo.co.uk>
2023-04-13 14:59:57 +0530

    added securtity group

--- main.tf
+++ main.tf
@@ -22,3 +22,40 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_vpc" "default" {
+  default : true
+}
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = "t3.nano"
+  vpc_security_group_ids = [aws_security_group.blog.id]
+  tags                   = {
+    Name = "Learning platform"
+  }
+}
+
+resource "aws_security_group" "blog" {
+  name        = "blog",
+  description = "Allow http and Https in Allow anything in"
+
+  vpc_id = aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.blog.id
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.blog.id
+  to_port           = 443
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
